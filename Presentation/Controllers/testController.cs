ca65b4360ce504a358ff16c8d0f7f53bf3e50852
DevX <christian.bx100pre@gmail.com>
2023-11-22 13:28:15 -0500

    add test controller

---
+++ Presentation/Controllers/testController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("Todo ok");
+        }
+    }
+}
