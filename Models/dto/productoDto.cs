5b121130829588de37bee823075f9c871c1b6947
DevX <christian.bx100pre@gmail.com>
2023-12-09 01:07:48 -0500

    optimizacion producto consulta

--- Models/dto/productoDto.cs
+++ Models/dto/productoDto.cs
@@ -27,5 +27,13 @@ namespace Models.dto
         public virtual List<colorProductoDto>? colorProducto { get; set; }
         public virtual List<imgProductoDto>? imgProducto { get; set; }
         public virtual List<tallaProductoDto>? TallaProducto { get; set; }
+
+        /*
+            -- Crear un índice en la columna id_producto de la tabla colorProducto
+            -- CREATE INDEX IDX_ColorProducto_IdProducto ON colorProducto (id_producto)
+            -- CREATE INDEX IDX_ImgProducto_IdProducto ON imgProducto (id_producto)
+            -- CREATE INDEX IDX_TallaProducto_IdProducto ON tallaProducto (id_producto)
+            -- CREATE INDEX IDX_ColorTalla_IdTalla ON tallaColor (id_talla)
+         */
     }
 }
--- Models/imgProductoModel.cs
+++ Models/imgProductoModel.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -14,6 +16,7 @@ namespace Models
     {
         [Key]
         public Int64 id  { get; set; }
+
         public int id_producto { get; set; }
         public string? nombreImg { get; set; }
         public string? imgUrl { get; set; }
